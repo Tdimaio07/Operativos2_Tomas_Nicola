46eebb8ff0ef136d8bc01d9302deafa88f1010b0
Tomas Dimaio <t.dimaio@correo.unimet.edu.ve>
2025-03-14 00:11:40 -0400

    Iniciar Proyecto

---
+++ Operativo2/src/main/java/com/mycompany/operativo2/Operativo2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.operativo2;
+
+/**
+ *
+ * @author tomas
+ */
+public class Operativo2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
